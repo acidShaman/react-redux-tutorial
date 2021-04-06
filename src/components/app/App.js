968d5af7807207467750677f612266cf69fbe30b
AndriyYurchuk <paltashka50@gmail.com>
2021-04-06 17:03:25 +0300

    Item add form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,68 @@
-import '../../styles/App.css';
+import React, { Component } from 'react';
+import '../../styles/app.css';
 import Header from '../header'
-import ToDoList from '../todo-list/ToDoList'
-import SearchPanel from '../search-panel/SearchPanel'
-import ItemStatusFilter from '../item-status-filter/ItemStatusFilter';
+import ToDoList from '../todo-list'
+import SearchPanel from '../search-panel'
+import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form'
 
-function App() {
+export default class App extends Component {
 
-  const toDoData = [
-    {id: 1, label: 'Drink cofee', important: false},
-    {id: 2, label: 'Sve the World!', important: true},
-    {id: 3, label: 'Do smth else!', important: false}
-  ]
+  maxId = 100
 
-  return (
-    <div>
-      <Header done={3} toDo={2}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    toDoData: [
+      {id: 1, label: 'Drink cofee', important: false},
+      {id: 2, label: 'Sve the World!', important: true},
+      {id: 3, label: 'Do smth else!', important: false}
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState(({toDoData}) => {
+      const index = toDoData.findIndex((el) => el.id === id)
+      const newArray = [...toDoData.slice(0, index), ...toDoData.slice(index + 1)]
+      return {
+        toDoData: newArray
+      }
+    })
+  }
+
+  addItem = (text) => {
+    console.log('Added', text)
+
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    } 
+
+    this.setState(({toDoData}) => {
+      const newData = [...toDoData, newItem]
+      return {
+        toDoData: newData
+      }
+    })
+
+
+  }
+
+  render () {
+    return (
+      <div>
+        <Header done={3} toDo={2}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <ToDoList 
+          todos={this.state.toDoData} 
+          onDeleted={this.deleteItem}
+        />
+
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
-      <ToDoList 
-        todos={toDoData} 
-        onDeleted={(id) => console.log(id)}
-      />
-    </div>
+    );
+  }
 
-  );
 }
-
-export default App;
